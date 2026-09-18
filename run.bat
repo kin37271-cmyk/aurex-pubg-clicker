86d4b0fc851357ae3fb5fad2596be809d6ce6ac8
kin37271-cmyk <kin37271-cmyk@users.noreply.github.com>
2026-09-18 18:43:24 +0500

    Update pre-populated SQLite DB with admin 8422157752 and add run.bat

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+title Aurex PUBG Clicker Bot
+echo ==============================================
+echo       Aurex PUBG Clicker Bot ishga tushmoqda...
+echo ==============================================
+py main.py
+pause
